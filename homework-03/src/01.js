704691f032b86ea59c5b5f62fbcac94d141dcb3a
Yevhenko <yevhenkozlyi@gmail.com>
2019-11-30 11:37:32 +0200

    homework-03 recommitted

--- homework-03/src/01.js
+++ homework-03/src/01.js
@@ -1,20 +1,20 @@
-let prime = 2;
+let prime = 1;
 
 // eslint-disable-next-line consistent-return
-function getNextPrime(x) {
-  let num = x;
-
-  while (num) {
-    for (let div = 1; div < num; div += 1) {
-      // eslint-disable-next-line no-continue
-      if (num % div !== 0) continue;
-      num += 1;
+function getNextPrime(currentPrime) {
+  let pr = currentPrime + 1;
+  while (pr) {
+    let isPrime = true;
+    for (let num = 2; num < pr; num += 1) {
+      if (pr % num === 0) {
+        isPrime = false;
+        break;
+      }
     }
-
-    return num;
+    if (isPrime === true) return pr;
+    pr += 1;
   }
 }
-
 setInterval(() => {
   console.log(`${Date.now()}: -- IN PROCESS -- Biggest prime number found: ${prime}`);
 }, 1000);
@@ -22,4 +22,4 @@ setInterval(() => {
 setInterval(() => {
   prime = getNextPrime(prime);
   console.log(prime);
-}, 50);
+}, 5);
