f4fdd62038c5d4f7a1693d3f0239de09ef5af5ac
Yevhenko <yevhenkozlyi@gmail.com>
2019-12-10 21:40:39 +0200

    homework-04 corrected

--- homework-04/src/02.js
+++ homework-04/src/02.js
@@ -1,23 +1,30 @@
 function randomInteger(max) {
-  return Math.floor(Math.random() * max) + 1;
+  return Math.floor(Math.random() * max);
 }
 
-function throwDice() {
-  return Promise.resolve(randomInteger(6));
-}
-
-setTimeout(() => {
-  throwDice().then((dice1) => {
-    console.log(`The first dice is ${dice1}`);
-
+async function throwDice(time) {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      throwDice().then((dice2) => {
-        console.log(`The second dice is ${dice2}`);
-
-        setTimeout(() => {
-          console.log(`The sum of dices is ${dice1 + dice2}`);
-        }, 3000);
-      });
-    }, 2000);
+      resolve();
+    }, time);
   });
-}, 700);
+}
+
+let sum = 0;
+
+Promise.resolve()
+  .then(() => throwDice(700).then(() => randomInteger(6)))
+  .then((dice1) => {
+    if (dice1 === 0) throw new Error('Lost dice');
+    console.log(`The first dice is ${dice1}`);
+    sum += dice1;
+  })
+  .then(() => throwDice(2000).then(() => randomInteger(6)))
+  .then((dice2) => {
+    if (dice2 === 0) throw new Error('Lost dice');
+    console.log(`The second dice is ${dice2}`);
+    sum += dice2;
+  });
+setTimeout(() => {
+  console.log(`The sum of dices is ${sum}`);
+}, 3000);
